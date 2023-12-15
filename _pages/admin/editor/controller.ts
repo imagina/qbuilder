f41b469b7656da93b865e2ef59a28b7346b80742
Noith <nimoba101@gmail.com>
2023-12-15 11:45:31 -0500

    Added: save layout and blocks

--- _pages/admin/editor/controller.ts
+++ _pages/admin/editor/controller.ts
@@ -3,7 +3,7 @@ import service from '@imagina/qbuilder/_pages/admin/editor/services'
 import store from '@imagina/qbuilder/_pages/admin/editor/store'
 
 export default function editorController() {
-  const proxy = getCurrentInstance().proxy
+  const proxy = getCurrentInstance()!.proxy
 
   // Refs
   const refs = {
@@ -12,7 +12,8 @@ export default function editorController() {
 
   // States
   const state = reactive({
-    layoutTab: 'preview'
+    layoutTab: 'preview',
+    loading: false
   })
 
   // Computed
@@ -32,7 +33,64 @@ export default function editorController() {
           )
         }, 300)
       }
-    }
+    },
+    changeLayout(value) {
+      const {layout, select} = value
+      if(store.layoutSelected && store.layoutSelected.id !== layout.id) {
+        proxy.$alert.warning({
+          mode: 'modal',
+          title: proxy.$tr('ibuilder.cms.label.sureChangeLayout'),
+          message: proxy.$tr('ibuilder.cms.label.descriptionSureChangeLayout'),
+          actions: [
+            {label: proxy.$tr('isite.cms.label.cancel'), color: 'grey-8'},
+            {
+              label: proxy.$tr('isite.cms.label.accept'),
+              color: 'green',
+              handler: () => {
+                store.layoutSelected = layout
+                select(layout)
+              }
+            },
+          ]
+        })
+      } else {
+        store.layoutSelected = layout
+        select(layout)
+      }
+    },
+    saveLayout() {
+      state.loading = true
+      const layout = store.layoutSelected
+
+      proxy.$crud.update('apiRoutes.qbuilder.layouts', layout.id, layout).then(response => {
+        proxy.$alert.info({message: proxy.$tr('isite.cms.message.recordUpdated')})
+        methods.saveBlocks(layout.blocks)
+      }).catch(error => {
+        proxy.$alert.error({message: proxy.$tr('isite.cms.message.recordNoUpdated')})
+        state.loading = false
+      })
+    },
+    saveBlocks(blocks) {
+      const requestParams = {notToSnakeCase: ["component", "entity", "attributes"]}
+      for (let i = 0; i < blocks.length; i++) {
+        const block = blocks[i];
+
+        proxy.$crud.update('apiRoutes.qbuilder.blocks', block.id, block, requestParams).then(response => {
+          proxy.$alert.info({message: proxy.$tr('isite.cms.message.recordUpdated')});
+          // Verificar si es el último ciclo
+          if (i === blocks.length - 1) {
+            state.loading = false;
+          }
+        }).catch(error => {
+          proxy.$alert.error({message: proxy.$tr('isite.cms.message.recordNoUpdated')});
+          // En caso de error, también considerarlo como el último ciclo
+          if (i === blocks.length - 1) {
+            state.loading = false;
+          }
+        });
+      }
+
+    },
   }
 
   // Mounted

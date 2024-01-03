7842daee8b040836a47e4f49dd4171712647a4b8
Noith <nmontealegre@imaginacolombia.com>
2024-01-03 08:09:36 -0500

    Style the component blocksPanel

--- _components/blocksPanel/index.vue
+++ _components/blocksPanel/index.vue
@@ -6,7 +6,7 @@
     <!--Block types-->
     <div class="row">
       <div class="col-4">
-        <q-tabs v-model="blockTypeSelected" vertical class="text-teal">
+        <q-tabs v-model="blockTypeSelected" vertical class="tabs">
           <q-tab v-for="(type, keyItem) in blockTypes" :key="keyItem" :name="type.systemName" :label="type.title" no-caps/>
         </q-tabs>
       </div>
@@ -18,18 +18,18 @@
           <div class="q-pa-md text-center text-weight-bold">{{ $tr('ibuilder.cms.label.localBlocks') }}</div>
           <q-item v-for="(block, blockKey) in blocksBySelectedType.local" :key="`localKey${blockKey}`" clickable
                   v-ripple active-class="list-selected" @click="selectBlock(block)">
-            <q-item-section :class="`q-ma-sm selectable ${blockSelected && blockSelected.id == block.id && blockSelected.blockType === 'local' ? 'selectable--selected': ''}`">
+            <q-item-section :class="`relative-position q-ma-sm selectable ${blockSelected && blockSelected.id == block.id && blockSelected.blockType === 'local' ? 'selectable--selected': ''}`">
               <img :src="block.mediaFiles.blockbgimage.path" :alt="block.internalTitle" />
-              <span class="ellipsis-2-lines full-width">{{ block.internalTitle}}</span>
+              <span class="ellipsis-2-lines full-width title-item q-py-xs">{{ block.internalTitle}}</span>
             </q-item-section>
           </q-item>
           <q-separator />
           <div class="q-pa-md text-center text-weight-bold">{{ $tr('ibuilder.cms.label.libraryBlocks') }}</div>
           <q-item v-for="(block, blockKey) in blocksBySelectedType.library" :key="`libraryKey${blockKey}`" clickable
                   v-ripple active-class="list-selected" @click="selectBlock(block, 'library')">
-            <q-item-section :class="`q-ma-sm selectable ${blockSelected && blockSelected.id == block.id && blockSelected.blockType === 'library' ? 'selectable--selected': ''}`">
+            <q-item-section :class="`relative-position q-ma-sm selectable ${blockSelected && blockSelected.id == block.id && blockSelected.blockType === 'library' ? 'selectable--selected': ''}`">
               <img :src="block.mediaFiles.blockbgimage.path" :alt="block.internalTitle" />
-              <span class="ellipsis-2-lines full-width">{{ block.internalTitle}}</span>
+              <span class="ellipsis-2-lines full-width title-item q-py-xs">{{ block.internalTitle}}</span>
             </q-item-section>
           </q-item>
         </div>
@@ -64,4 +64,30 @@ export default defineComponent({
     background-color $primary
     color white
   }
+
+  .tabs {
+    .q-tab--active {
+      background $primary
+      color white
+    }
+  }
+
+  .selectable {
+    &:hover .title-item,
+    &.selectable--selected .title-item {
+      opacity 1
+    }
+
+    .title-item {
+      opacity 0
+      position absolute
+      bottom 0
+      background rgba(0, 0, 0, 0.75)
+      display grid
+      place-items center
+      color white
+    }
+  }
+
+
 </style>

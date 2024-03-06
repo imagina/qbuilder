c5dd84d821cef6a7ddd33948273141a9da1e58f4
IMNicolasM <143624699+IMNicolasM@users.noreply.github.com>
2024-03-06 10:59:38 -0500

    Fixed: change service to get info of library (#72)
    * Fixed: icon and permission of editor
    
    * Fixed: change service to get info of library

--- _components/layoutLibraryPanel/services.ts
+++ _components/layoutLibraryPanel/services.ts
@@ -1,5 +1,6 @@
 import baseService from '@imagina/qcrud/_services/baseService'
 import {Layout} from '@imagina/qbuilder/_components/layoutPanel/interface'
+import {getCurrentInstance} from "vue";
 
 interface ResponseGetLayout {
   data: Layout[]
@@ -8,12 +9,20 @@ interface ResponseGetLayout {
 
 export default {
   getLayoutsLibrary(refresh = false, params = {}): Promise<Layout[]> {
+    const proxy = getCurrentInstance()!.proxy
     return new Promise((resolve, reject) => {
-      const requestParams = {refresh, params}
+      const baseUrl = proxy.$store.getters['qsiteApp/getSettingValueByName']('ibuilder::blockTemplatesUrl')
+      if(!baseUrl) return resolve([])
+      const requestParams = {
+        filter: {allTranslations: true},
+        refresh,
+        ...params
+      }
 
       //Request
-      baseService.index<ResponseGetLayout>('apiRoutes.qbuilder.layouts', requestParams).then((response) => {
-        resolve(response.data)
+      //@ts-ignore
+      proxy.$axios.get<ResponseGetLayout>(`${baseUrl}/api${config('apiRoutes.qbuilder.layouts')}`, requestParams).then((response) => {
+        resolve(response.data.data)
       }).catch(error => reject(error))
     })
   },

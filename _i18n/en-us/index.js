543a8dde977e64cd8bbc90420228bd847a4d489f
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2024-06-27 11:37:15 -0500

    + added translations (#89)

---
+++ _i18n/en-us/index.js
@@ -0,0 +1,57 @@
+export default {
+  "ibuilder": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Builder",
+        "adminBlocks": "Blocks",
+        "adminTemplates": "Templates"
+      },
+      "newBlock": "New Block",
+      "updateBlock": "Update Block",
+      "block": "Block | Blocks",
+      "chooseBlock": "Choose a Block",
+      "layout": "Layout | Layouts",
+      "newLayout": "Create New Layout",
+      "layouts": "Layouts",
+      "blockEdit": "Block Edit",
+      "label": {
+        "grid": "Grid",
+        "preview": "Preview",
+        "chooseLayout": "Choose a layout",
+        "newLayout": "New Layout",
+        "updateLayout": "Update Layout",
+        "sureChangeLayout": "Do you want to change the layout?",
+        "descriptionSureChangeLayout": "You are going to change the layout, if you have unsaved changes they will be lost",
+        "blocks": "Blocks",
+        "localBlocks": "Locals",
+        "libraryBlocks": "Library",
+        "system": "System",
+        "sortPosition": "Sort Position",
+        "blockContent": "Block Content",
+        "title": "Title",
+        "subtitle": "Subtitle",
+        "bannerMessage": "Select a unique 'System Name' that identifies the block and then the component you want to customize...",
+        "content": "Content",
+        "attributes": "Attributes",
+        "discard": "Discard",
+        "apply": "Apply",
+        "editBlockAttributes": "Edit block attributes",
+        "sureDiscardBlock": "Have you saved your changes?",
+        "descriptionSureDiscardBlock": "If you continue with this action, you will lose all your progress. Take a moment to make sure!",
+        "sureRefreshLayout": "Do you want to reload the layouts?",
+        "descriptionSureRefreshLayout": "Be careful! If you have unsaved changes, they will be lost when you refresh",
+        "administrator": "Go to administrator",
+        "sureChangeView": "Are you sure about this action?",
+        "chooseBlock": "Choose a Block",
+        "clone": "Clone",
+        "relate": "Relate"
+      },
+      "form": {
+        "layoutEntityType": "Defines the context or category to which the layout belongs",
+        "layoutType": "Specifies the presentation type within the selected entity",
+        "layoutDefault": "Indicates whether the layout is set as default for the corresponding entity and type",
+        "layout": "Builder Layout"
+      }
+    }
+  }
+}
---
+++ _i18n/es/index.js
@@ -0,0 +1,58 @@
+export default {
+  "ibuilder": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Constructor",
+        "adminBlocks": "Bloques",
+        "adminTemplates": "Plantillas"
+      },
+      "newBlock": "Nuevo Bloque",
+      "updateBlock": "Actualizar Bloque",
+      "block": "Bloque | Bloques",
+      "blockDesign": "Diseño del Bloque",
+      "chooseBlock": "Selecciona un bloque",
+      "layout": "Layout | Layouts",
+      "newLayout": "Crea un nuevo Layout",
+      "layouts": "Layouts",
+      "blockEdit": "Editar bloque",
+      "label": {
+        "grid": "Grilla",
+        "preview": "Vista Previa",
+        "chooseLayout": "Escoge un Layout",
+        "newLayout": "Nuevo Layout",
+        "updateLayout": "Actualizar Layout",
+        "sureChangeLayout": "¿Deseas cambiar de layout?",
+        "descriptionSureChangeLayout": "Vas a cambiar de layout, si tienes cambios sin guardar se perderan",
+        "blocks": "Bloques",
+        "localBlocks": "Locales",
+        "libraryBlocks": "Libreria",
+        "system": "Sistema",
+        "sortPosition": "Posición en la Grilla",
+        "blockContent": "Contenido del Bloque",
+        "title": "Titulo",
+        "subtitle": "Subtitulo",
+        "bannerMessage": "Selecciona un 'Nombre de Sistema' unico que identifique el bloque y luego el componente que quieres perzonalizar...",
+        "content": "Contenido",
+        "attributes": "Atributos",
+        "discard": "Descartar",
+        "apply": "Aplicar",
+        "editBlockAttributes": "Editar los atributos del bloque",
+        "sureDiscardBlock": "¿Has guardado tus cambios?",
+        "descriptionSureDiscardBlock": "Si continuas con esta acción, perderás todo tu progreso. ¡Tómate un momento para asegurarte!",
+        "sureRefreshLayout": "¿Deseas volver a cargar los layouts?",
+        "descriptionSureRefreshLayout": "¡Ten cuidado! Si tienes cambios sin guardar, se perderán al refrescar",
+        "administrator": "Ir al administrador",
+        "sureChangeView": "¿Está seguro de esta acción?",
+        "chooseBlock": "Escoge un Bloque",
+        "clone": "Clonar",
+        "relate": "Relacionar"
+      },
+      "form": {
+        "layoutEntityType": "Define el contexto o categoría al que pertenece el layout",
+        "layoutType": "Especifica el tipo de presentación dentro de la entidad seleccionada",
+        "layoutDefault": "Indica si el layout se establece como predeterminado para la entidad y el tipo correspondiente",
+        "layout": "Builder Layout"
+      }
+    }
+  }  
+}
